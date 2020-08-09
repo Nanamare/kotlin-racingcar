56a9f1da2c1355e1cc3669b64ffa100dfa999347
hyunsung.shin <cordorshs@gmail.com>
2020-08-09 23:02:19 +0900

    feat(RacingCar) : Position 내부 외부 분리

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,9 +2,14 @@ package racingcar
 
 import racingcar.strategy.Strategy
 
-class Car(val position: MutableList<Int>, private val strategy: Strategy) {
-    fun forward() = position.add(position.last() + 1)
-    fun wait() = position.add(position.last())
+class Car(position: List<Int>, private val strategy: Strategy) {
+
+    val position
+        get() = _position
+    private val _position = position.toMutableList()
+
+    fun forward() = _position.add(_position.last() + 1)
+    fun wait() = _position.add(_position.last())
 
     fun run() {
         when (strategy.move(strategy.generateNumber())) {
